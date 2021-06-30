762078d306f4f1b54a9ea51ee5429655f2981e60
Piero <piero.aldinucci@gmail.com>
2021-06-30 21:00:31 +0200

    Added Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/greeting',function (){
-    return 'Hello World';
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
